f45746ecd5ccfb3aa2e30f29643c9e538bbb78f7
Yash SHARMA <yash221b@gmail.com>
2020-07-08 21:17:08 +0200

    added some functions

--- README.md
+++ README.md
@@ -5,3 +5,24 @@ To add the MinilibX to your system you can copy:
 libmlx.a in /usr/local/lib
 mlx.h in /usr/local/include
 man/man3/mlx*.1 in /usr/local/man/man3
+
+
+
+
+
+
+
+
+//helpful links
+
+#For RayCasting
+https://lodev.org/cgtutor/raycasting.html
+https://lodev.org/cgtutor/files/raycaster_flat.cpp
+https://lodev.org/cgtutor/files/raycaster_textured.cpp
+
+#For Mathematics
+http://projet-moteur-3d.e-monsite.com/pages/raycasting/raycasting.html
+
+#For MinilibX
+https://harm-smits.github.io/42docs/libs/minilibx/getting_started.html
+
--- cub3d.c
+++ cub3d.c
@@ -1,46 +1,34 @@
 #include "minilibx/mlx.h"
 #include <stdio.h>
 
-int main (void)
-{
-    void *img;
-    void *mlx_ptr;
-    void *win_ptr;
+typedef	struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
-    mlx_ptr = mlx_init();
-    if (!mlx_ptr)
-        printf ("NULL returned. No connection established");
-    img = mlx_new_image(mlx_ptr, 1920, 1080);
+void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
 }
 
 
+int				main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
 
-
-
-
-
-
-
-
-
-
-
-    // void *mlx_ptr;
-    // void *win_ptr;
-    // int i;
-    // int j;
-
-    // mlx_ptr = mlx_init();
-    // if (!mlx_ptr)
-    //     printf ("NULL returned. No connection established");
-
-    // win_ptr = mlx_new_window(mlx_ptr, 1024, 1024, "hello world");
-    // i = 100;
-    // j = 100;
-    // while (++i <= 600)
-    // {
-    //     while (++j <= 600)
-    //         mlx_pixel_put(mlx_ptr, win_ptr, i , j, 0x0000FF00);
-    //     j = 100;
-    // }
-    // mlx_loop(mlx_ptr);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
