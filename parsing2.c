cee75c8dffc6456979516d44d5c4a0530ed091b3
Yash SHARMA <yash221b@gmail.com>
2020-07-12 08:54:47 +0200

    added comments for future reference

--- parsing2.c
+++ parsing2.c
@@ -12,6 +12,12 @@ t_parse		*parse_data_init(t_parse *p_data)
 t_parse		*parse_map(char *line, t_parse *p_data, int spaces)
 {
 	printf("x - %d\n", p_data->map_x);
+	
+	//need to malloc the map
+	//need to know exact number of Y in order to do it
+	//map_x and map_y are to malloc the map exactly as it is
+	
+	
 	while (spaces-- > 0)
 		printf("*");
 	printf ("%s\n", line);
